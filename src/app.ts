dd25806fa6274d1aaba4d23fc000834798d4c9b9
abelpormade <abel@pormade.com.br>
2025-09-02 16:58:32 -0300

    aaa

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+/*Agora, você pode rodar seu servidor em modo desenvolvimento com:
+
+npm run dev
+
+
+Ele vai iniciar seu src/app.ts e ficar assistindo mudanças, recarregando automaticamente.
+
+Para compilar o TypeScript, rode:
+
+npm run build
+
+
+E para rodar o app compilado:
+
+npm start*/
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Olá, TheKick.com!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
